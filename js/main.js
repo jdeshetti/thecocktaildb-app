1dde633fc7a1b0a04514ce06e942fdf89186c50c
Sampath Deshetti <sampathdeshetti@gmail.com>
2023-12-08 08:10:14 +1100

    few more changes in css style and wrote js code to render drink details

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ console.log( axios );
 const cocktailSearch = {
 
   config: {
-    COCKTAILDB_BASE_URL: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?'
+    COCKTAILDB_BASE_URL: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?',
+    COCKTAILDB_DETAILED_URL: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?'
   },
 
   dom: {},
@@ -28,19 +29,23 @@ const cocktailSearch = {
       ev.preventDefault(); //to stop reloading the page
       this.loadSearchResults( this.dom.searchText.value);
     })//to submit an event in serachForm
+
+    this.dom.searchText.focus();
+
+    this.dom.searchResults.addEventListener('click', ev => {
+      console.log(`clicked image:`, ev.target.dataset.id);//to check the ID is displaying?
+      this.loadCocktailDetails( ev.target.dataset.id);
+      
+    }); //cocktailimg click result handler
+
+
+
+
   }, // end of initUi function
 
   loadSearchResults( searchText ){
 
     console.log( `Text in loadsearchResults:`, searchText );//to check the serachForm is taking input or not
-
-
-
-
-
-
-
-
 
     axios.get( this.config.COCKTAILDB_BASE_URL, {
       params: {
@@ -70,15 +75,19 @@ const cocktailSearch = {
       console.log( drink.strDrink);//to check the title of the drink in the console
       console.log( drink.strDrinkThumb);//to check the thumbnilimg of the drink in the console
 
-      const drinkContainer = document.createElement('div'); // Create a container for each drink
+      const drinkContainer = document.createElement('div'); // Created a container for each drink
+      drinkContainer.classList.add('drink-container');
     
-      const titleNode = document.createElement('div'); // Create a div for the title
+      const titleNode = document.createElement('div'); // Created a div for the title
       titleNode.textContent = drink.strDrink; // Set the drink title text
-      titleNode.classList.add('drink-title'); // Add a class for styling if needed
+      titleNode.classList.add('drink-title'); // Added a class for styling 
     
-      const imgNode = document.createElement('img');
-      imgNode.src = drink.strDrinkThumb + '/preview';
+      const imgNode = document.createElement('img');//for the image
+      imgNode.src = drink.strDrinkThumb + '/preview';//image source
       imgNode.alt = drink.strDrink;
+
+      //we know the drinkid here and so we can use it to get ID when img clciked
+      imgNode.dataset.id = drink.idDrink;
     
       drinkContainer.appendChild(titleNode); // Append title before the image
       drinkContainer.appendChild(imgNode); // Append the image
@@ -90,6 +99,56 @@ const cocktailSearch = {
     } // each drink
 
   }, // end of renderSearchResults
+
+  
+  loadCocktailDetails( id ){
+
+    console.log( `the ID of individual cocktail:`, id );
+
+    axios.get( this.config.COCKTAILDB_DETAILED_URL, {
+      params: {
+        i: id
+
+      }
+    })
+      .then( res => {
+        console.log( 'data:', res.data);//to check the drinks data
+
+      })
+      .catch( err => {
+        console.warn('Please search again with the correct drink name', err );
+        //to display message to user in DOM if no serach was sfound
+      });
+
+
+
+
+
+
+  }, //end of loadCockTailDetails
+
+
+
+  /*renderCocktailDetails( drink ){
+
+    this.dom.searchResults.style.display = 'none';
+
+    this.dom.cocktailDetails.style.display = 'block'; // unhide
+    this.dom.cocktailDetails.replaceChildren(); // clear
+
+    const headingTag = document.createElement( 'h2' );
+    headingTag.innerHTML = drink.strDrink;
+    this.don.cocktailDetails.appendChild( headingTag );
+
+
+
+
+
+
+
+
+
+  },//end of renderCocktailDetails*/
 
 
 
